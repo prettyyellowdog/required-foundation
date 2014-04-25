6cf57fe24d8e2af99adc4d37d99fc17cf7d797f2
Greg Thomson <greg.thom47>
2014-04-25 15:55:34 -0700

    Top Bar
    Fix top bar so it works and also remove search bar from it.

--- nav.php
+++ nav.php
@@ -12,17 +12,17 @@
 ?>
 
     <?php do_action('required_before_nav'); ?>
-        <nav class="top-bar">
+        <nav class="top-bar" data-topbar>
             <ul class="title-area">
                 <li class="name"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1></li>
-                <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+                <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
             </ul>
             <section class="top-bar-section">
             <?php
                 wp_nav_menu( array(
                     'theme_location' => 'primary',
                     'depth' => 0,
-                    'items_wrap' => '<ul class="left">%3$s</ul>',
+                    'items_wrap' => '<ul class="right">%3$s</ul>',
                     'container' => '',
                     'fallback_cb' => 'required_menu_fallback', // workaround to show a message to set up a menu
                     'walker' => new REQ_Foundation_Walker( array(
@@ -31,10 +31,7 @@
                     ) ),
                 ) );
             ?>
-                <ul class="right">
-                    <li class="divider hide-for-small"></li>
-                    <li class="has-form"><?php get_search_form(); ?></li>
-                </ul>
+                
             </section>
         </nav>
     <?php do_action('required_after_nav'); ?>
